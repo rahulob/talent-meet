ac32d7feac21098230ba8f1aa7419dab12fc243d
rahulob <rahulob@proton.me>
2025-12-02 21:28:37 +0530

    Auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,40 @@
-import express from 'express';
-import {ENV} from "./lib/env.js";
-import { connectDB } from './lib/db.js';
+import express from "express";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { inngest } from "./lib/inngest.js";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true, // Allows browser to send cookies
+  })
+);
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: functions,
+  })
+);
 
-app.get('/health', (req, res) => {
-    res.status(200).json({msg: "Api is up and running"});
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "Api is up and running" });
+});
 
 const startServer = async () => {
-    try {
-        await connectDB();
-        app.listen(ENV.PORT, ()=>{
-            console.log(`✅ Server started on port ${ENV.PORT}`);
-        })
-    } catch (error) {
-        console.log("❌ Error starting server", error);
-        process.exit(1);
-    }
-}
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`✅ Server started on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.log("❌ Error starting server", error);
+    process.exit(1);
+  }
+};
 
-startServer(); 
+startServer();
