7b8686d5d65c160e8a048de1e096bb48bf2d48c6
Rahulob <rahulob@rahulob.local>
2025-12-02 15:55:56 +0530

    Database setup done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {ENV} from "./lib/env.js";
+import { connectDB } from './lib/db.js';
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,16 @@ app.get('/health', (req, res) => {
     res.status(200).json({msg: "Api is up and running"});
 })
 
-app.listen(ENV.PORT, ()=>{
-    console.log(`Server started on port ${ENV.PORT}`);
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, ()=>{
+            console.log(`✅ Server started on port ${ENV.PORT}`);
+        })
+    } catch (error) {
+        console.log("❌ Error starting server", error);
+        process.exit(1);
+    }
+}
+
+startServer(); 
