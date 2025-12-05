9d096330048ddd1b37c962c44f96f7243d340e9a
rahulob <rahulob@proton.me>
2025-12-05 18:13:28 +0530

    Refactoring session controller

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -113,15 +113,22 @@ export async function joinSession(req, res) {
     const session = await Session.findById(id);
 
     if (!session) return res.status(404).json({ message: "Session not found" });
+    if (session.status === "completed")
+      return res.status(400).json({ message: "Can't join completed session" });
+
+    if (session.host.toString() === userId.toString)
+      return res
+        .status(400)
+        .json({ message: "Host cannot join as participant" });
     // check if participant is already there
     if (session.participant)
-      return res.status(404).json({ message: "Session is full" });
-
-    session.participant = userId;
-    await session.save();
+      return res.status(409).json({ message: "Session is full" });
 
     const channel = chatClient.channel("messaging", session.callId);
     await channel.addMembers([clerkId]);
+
+    session.participant = userId;
+    await session.save();
 
     res.status(200).json({ session });
   } catch (err) {
